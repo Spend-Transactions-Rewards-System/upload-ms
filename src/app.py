f0b223dc18bb8d51f4a858bc17b559131b9ebbc3
Sean Tan <seantan1710@gmail.com>
2023-03-10 13:20:08 +0800

    Enabled CORS

--- src/app.py
+++ src/app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from src.view.upload import upload
 from src.view.download import download
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(upload, url_prefix="/api/v1/upload")
 app.register_blueprint(download, url_prefix="/api/v1/download")
