ca707beec3ec50649bd5c62b7f430c54fc2dadbf
Sean Tan <seantan1710@gmail.com>
2023-03-24 02:39:14 +0800

    Changed port from 8080 to 80

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,4 @@
 from src.app import app
 
 if __name__ == "__main__":
-    ####################
-    # FOR DEVELOPMENT
-    ####################
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=80)
