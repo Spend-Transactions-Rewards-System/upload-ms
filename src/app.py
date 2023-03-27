c63c9d923fb9d91c24d0237d17a5bd3b721fa21a
Sean Tan <seantan1710@gmail.com>
2023-03-27 21:35:10 +0800

    Added healthcheck endpoint for app

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from src.view.upload import upload
 from src.view.download import download
 from src.view.healthcheck import healthcheck
+from src.model.jsonResponse import JsonResponse
 from dotenv import load_dotenv
 from flask_cors import CORS
 
@@ -10,6 +11,10 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/", methods=(["GET"]))
+def app_healthcheck():
+    return JsonResponse("Healthy", 200).send_response()
+
 app.register_blueprint(upload, url_prefix="/api/v1/upload")
 app.register_blueprint(download, url_prefix="/api/v1/download")
-app.register_blueprint(healthcheck, url_prefix="/")
+app.register_blueprint(healthcheck, url_prefix="/api/v1")
--- src/view/healthcheck.py
+++ src/view/healthcheck.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, request, send_file
+from flask import Blueprint
 from src.model.jsonResponse import JsonResponse
 
 healthcheck = Blueprint(name="healthcheck", import_name=__name__)
 
 
 @healthcheck.route("/", methods=(["GET"]))
-def healthcheck_ep():
+def healthcheck_endpoint():
     return JsonResponse("Healthy", 200).send_response()
