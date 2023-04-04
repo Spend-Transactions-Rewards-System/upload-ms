4053e4585db9a58aac1a5a4209d4d7891e104034
Gan Shao Hong <shaohong.g97@gmail.com>
2023-04-05 02:03:20 +0800

    revert pointer back to header - file.py

--- src/utils/files.py
+++ src/utils/files.py
@@ -11,7 +11,9 @@ file_schema = {
 
 
 def is_valid_file_schema(file, file_type):
+    last_pos = file.tell()
     file_data = file.readline().decode("utf-8")
+    file.seek(last_pos) # return pointer to header row
     fields = file_data.rstrip("\r\n").replace('\"', '').split(",")
     for field in fields:
         if field not in file_schema[file_type]:
